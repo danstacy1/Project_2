b99a78adea391d49f62d3572b3fa7ad8c4b9eee6
danstacy1 <danstacy1@gmail.com>
2022-07-20 11:25:02 -0400

    deployment

--- models/connection.js
+++ models/connection.js
@@ -2,30 +2,22 @@
 require('dotenv').config()
 
 // If you want to use mongoose, you have to require it.
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+// Fire off the connection to Mongo DB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 
-const DATABASE_URI = process.env.DATABASE_URI
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`);
+});
 
-const config = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
+mongoose.connection.on("error", (err) => {
+  console.log("Could not connect to MongoDB!", err);
+});
 
-// connecting our mongoDB to mongoose
-mongoose.connect(DATABASE_URI, config)
-
-mongoose.connection
-// handle the opening of the connection
-// running code block on open
-// console.logging a string
-    .on('open', () => console.log('Connected to Mongoose'))
-    // since we have opened a connection we have to close it.
-    // running a code block on close
-    .on('close', () => console.log('Disconnected from Mongoose'))
-    // handle any error that might happen
-    // running a code block on error
-    // using a console.error to see that error
-    .on('error', err => console.error(err))
 
 module.exports = mongoose
